20799fb15e53b5180eb69cad4c0ed44d6a7bcc7a
Arslan <arslanasghar@gmail.com>
2021-02-24 11:03:17 -0800

    3nd

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:socialapp/models/user.dart';
+import 'package:socialapp/pages/home.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:cached_network_image/cached_network_image.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -6,13 +11,18 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  TextEditingController searchController = TextEditingController();
+  Future<QuerySnapshot> searchResultsFuture;
+
   Container buildNoContent() {
     return Container(
-
       child: ListView(
         shrinkWrap: true,
         children: [
-          Image.asset('assets/images/search.png',height: 360,),
+          Image.asset(
+            'assets/images/search.png',
+            height: 360,
+          ),
           Text(
             'Find User',
             textAlign: TextAlign.center,
@@ -28,22 +38,59 @@ class _SearchState extends State<Search> {
     );
   }
 
-  AppBar buildSearchResult() {
+  handleSearch(String query) {
+    Future<QuerySnapshot> users = firestoreInstance
+        .collection('users')
+        .where("displayName", isGreaterThanOrEqualTo: query)
+        .get();
+    setState(() {
+      searchResultsFuture = users;
+    });
+  }
+
+  AppBar buildSearchField() {
     return AppBar(
       backgroundColor: Colors.white,
       title: TextFormField(
+        controller: searchController,
         decoration: InputDecoration(
-            hintText: "Search for user...",
-            filled: true,
-            prefixIcon: Icon(
-              Icons.account_box,
-              size: 28.0,
-            ),
-            suffixIcon: IconButton(
-              icon: Icon(Icons.clear),
-              onPressed: () {},
-            )),
+          hintText: "Search for user...",
+          filled: true,
+          prefixIcon: Icon(
+            Icons.account_box,
+            size: 28.0,
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: clearSearch,
+          ),
+        ),
+        onFieldSubmitted: handleSearch,
       ),
+    );
+  }
+
+  clearSearch() {
+    searchController.clear();
+  }
+
+  buildSearchResult() {
+    return FutureBuilder(
+      future: searchResultsFuture,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return CircularProgressIndicator();
+        }
+        List<SearchResult> searchResults = [];
+        snapshot.data.docs.forEach((doc) {
+          User user = User.fromDocument(doc);
+          SearchResult searchResult = SearchResult(user);
+          searchResults.add(searchResult);
+        });
+        return ListView(
+          children: searchResults,
+        );
+      },
     );
   }
 
@@ -51,15 +98,41 @@ class _SearchState extends State<Search> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
-      appBar: buildSearchResult(),
-      body: buildNoContent(),
+      appBar: buildSearchField(),
+      body:
+          searchResultsFuture == Null ? buildNoContent() : buildSearchResult(),
     );
   }
 }
 
 class SearchResult extends StatelessWidget {
+  final User user;
+
+  SearchResult(this.user);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Theme.of(context).accentColor.withOpacity(0.8),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => print('ontap'),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+              ),
+              title: Text(user.displayName,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+            subtitle: Text(user.username,style: TextStyle(color: Colors.white),),
+            ),
+          ),
+          Divider(
+            height: 2.0,
+            color: Colors.white54,
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class Upload extends StatefulWidget {
   @override
@@ -7,8 +8,97 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  File file;
+
+  handleTakePhoto() async {
+    Navigator.pop(context);
+    File file = await ImagePicker.pickImage(
+        source: ImageSource.camera, maxHeight: 680, maxWidth: 960);
+    setState(() {
+      this.file = file;
+    });
+  }
+
+  handleChooseFromGallery() async{
+
+    Navigator.pop(context);
+    File file = await ImagePicker.pickImage(
+        source: ImageSource.gallery );
+    setState(() {
+      this.file = file;
+    });
+  }
+
+  selectImage(parentContext) {
+    return showDialog(
+        context: parentContext,
+        builder: (context) {
+          return SimpleDialog(
+            title: Text('create post'),
+            children: [
+              SimpleDialogOption(
+                child: Text('photo with camera'),
+                onPressed: handleTakePhoto,
+              ),
+              SimpleDialogOption(
+                child: Text('Image from gallery'),
+                onPressed: handleChooseFromGallery,
+              ),
+              SimpleDialogOption(
+                child: Text('cancel'),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        });
+  }
+
+  Container buildSplashScreen() {
+    return Container(
+      color: Theme.of(context).accentColor.withOpacity(0.8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 28.0),
+            child: Image.asset(
+              'assets/images/upload.png',
+              height: 260,
+            ),
+          ),
+          GestureDetector(
+            onTap: () => selectImage(context),
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                height: 35,
+                width: 150,
+                decoration: BoxDecoration(
+                    color: Colors.deepOrangeAccent,
+                    borderRadius: BorderRadius.circular(7.0)),
+                child: Center(
+                  child: Text(
+                    'upload Image',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  buildUploadForm(){
+    return Text('File Uploaded');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('UPLOAD');
+    return file == null? buildSplashScreen():buildUploadForm();
   }
 }
